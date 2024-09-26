2fa98430c6c4891d71e0409818f8ecd4bc93b1b4
vichugoribeiro <victoribeiro.alunosenai@gmail.com>
2024-09-25 21:57:34 -0300

    Aulapiton

---
+++ Atividade1.py
@@ -0,0 +1,8 @@
+inteira = int(input('Digite um numero inteiro'))
+
+flutuante = float (input('Digite um numero flutuante'))
+
+palavra = input ('Digite uma palavra!')
+
+print(f'Valor inteiro {inteira} {flutuante} {palavra}')
+
---
+++ atividade2.py
@@ -0,0 +1,11 @@
+def divisao ()
+    try:
+        n1 = int (input("Digite um numero"))
+        n2 = int (input("Digite outro numero"))
+        divisao = n1/n2
+        return div
+    except ValueError:
+        return "divisao por 0"
+    
+resultado= divisao()
+print (Resultado)
